5493bbd53e8ce976c200f4fe4866032d35c53843
Cameron Webster <cmwebster90@googlemail.com>
2017-03-07 17:09:00 +0100

    addition to projects model

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,10 +5,18 @@ class Project < ApplicationRecord
   has_many :comments, through: :images
 
   validates :name, presence: true
+  validates :campaign_start, presence: true
   validates :deadline, presence: true
   validates :status, presence: true
   validates :brief, presence: true
+  validate :compare_date
+end
 
+def compare_date
+  return if campaign_start.nil? && deadline.nil?
+  if campaign_start < deadline
+    errors.add(:deadline, "can't be after the campaign_start")
+  end
 end
 
 
